456017632f788ef3ecbf5ae08ed2c1949dc61e34
VPEL <VPEL@192.168.0.197>
2023-01-12 16:14:52 +0530

    Yogesh 12012023 V1519

---
+++ Redis/src/main/java/com/vst/redis/repository/ProductRepo.java
@@ -0,0 +1,44 @@
+package com.vst.redis.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.vst.redis.entity.Product;
+
+@Repository
+@SuppressWarnings("unchecked")
+public class ProductRepo {
+	
+	@Autowired
+	private RedisTemplate template;
+	
+	public static final String HASH_KEY ="Product";
+	
+	public Product save(Product product)
+	{
+		template.opsForHash().put(HASH_KEY,product.getId(),product);
+		return product;
+	}
+	
+	public List<Product> findAll(){
+		return template.opsForHash().values(HASH_KEY);	
+	}
+	
+	public Product findProdcutById(int id) {
+		return (Product) template.opsForHash().get(HASH_KEY, id);
+	}
+	
+	public String deleteById(int id)
+	{
+		template.opsForHash().delete(HASH_KEY, id);
+		return "Product Deleted";	
+	}
+	
+	public Product updateById(int id,Product product) {
+		Product obj=(Product) template.opsForHash().get(HASH_KEY, id);
+		return obj;
+	}
+}
